e51fd2e0ff17703f8d51dd7d50464bbca42cdcc2
LizPeng <lizhipeng2013@164.com>
2018-02-05 15:37:56 +0800

    some problems

--- cp5_compose.js
+++ cp5_compose.js
@@ -1,23 +1,16 @@
 // https://llh911001.gitbooks.io/mostly-adequate-guide-chinese/content/ch5.html
-var compose = function (f, g) {
-  return function (x) {
-    return f(g(x))
-  }
-}
+const compose = (f, g) => x => f(g(x))
 var toUpperCase = function (x) {
   return x.toUpperCase()
 }
+
 var exclaim = function (x) {
   return x + '!'
 }
 var head = function (x) {
   return x[0]
 }
-var reverse = reduce(function (acc, x) {
-  return [x].concat(acc)
-}, [])
-
-
-var loudLastUpper = compose(exclaim, toUpperCase, head, reverse)
+// 有问题，只能compose两个
+var loudLastUpper = compose(exclaim, toUpperCase, head)
 
 loudLastUpper(['jumpkick', 'roundhouse', 'uppercut']);
--- cp5_pointfree.js
+++ cp5_pointfree.js
@@ -2,3 +2,6 @@
 var snakeCase = function(word) {
   return word.toLowerCase().replace(/\s+/ig, '_')
 }
+
+// pointfree
+var snakeCase = compose(replace(/\s+/ig, '_'), toLowerCase)
